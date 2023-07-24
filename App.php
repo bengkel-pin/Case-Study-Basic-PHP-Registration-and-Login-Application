4d1515ff501ea46a9453b3aee62f5da2e047d6e0
Kelvin <85025516+bengkel-pin@users.noreply.github.com>
2023-07-24 12:26:07 +0700

    Remove error line

--- App.php
+++ App.php
@@ -5,7 +5,6 @@ require_once "Model/Users.php";
 require_once "BussinessLogic/Login.php";
 require_once "BussinessLogic/RegisterUser.php";
 require_once "View/ViewRegister.php";
-require_once "View/ViewUser.php";
 require_once "View/ViewMenu.php";
 require_once "View/ViewLogin.php";
 
