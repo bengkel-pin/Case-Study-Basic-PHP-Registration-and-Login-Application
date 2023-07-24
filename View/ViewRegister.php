2f839b0b6b4ba19313261679193c84da90d30756
Kelvin <85025516+bengkel-pin@users.noreply.github.com>
2023-07-24 12:27:05 +0700

    Update the view to show the password requirements

--- View/ViewRegister.php
+++ View/ViewRegister.php
@@ -11,6 +11,13 @@ function viewRegister()
     while ($isNotDone) {
         echo "REGISTER" . PHP_EOL;
         $username = input("Create a new username");
+
+        echo "Password must contain: " . PHP_EOL;
+        echo "1. At least 8 characters" . PHP_EOL;
+        echo "2. At least one lowercase letter (a-z)" . PHP_EOL;
+        echo "3. At least one uppercase letter (A-Z)" . PHP_EOL;
+        echo "4. At least one number (0-9)" . PHP_EOL;
+        echo "5. At least one special character" . PHP_EOL;
         $password = input("Create a strong password");
 
         if (registerUser($username, $password)) {
