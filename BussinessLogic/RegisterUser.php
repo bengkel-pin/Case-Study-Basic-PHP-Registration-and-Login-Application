98d025445bd50593ac0422a7b15fb2ce87da05b2
Kelvin <85025516+bengkel-pin@users.noreply.github.com>
2023-07-24 12:23:41 +0700

    Add password validation

--- BussinessLogic/RegisterUser.php
+++ BussinessLogic/RegisterUser.php
@@ -1,19 +1,28 @@
 <?php
 
+require_once __DIR__ . "/../Helper/validatePassword.php";
+
 /**
  * Add new user to the array
  */
-function registerUser(string $username, string $password){
+function registerUser(string $username, string $password)
+{
 
      global $users;
-    
-     $lastQueue = sizeof($users);
 
      // Check if the username exist
      if (array_search($username, array_column($users, 'username')) !== false) {
           echo "Username is not available" . PHP_EOL;
           return false;
      }
+
+     //Validate the password
+     if (!validatePassword($password)){
+          echo "Password is not valid" . PHP_EOL;
+          return false;
+     }
+
+     $lastQueue = sizeof($users);
 
      $users[$lastQueue] = ["username" => $username, "password" => $password];
 
